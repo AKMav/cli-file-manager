6a64b81f6a6ad3fc22aff487443be8b37efd6ebc
Mavrayev <mavrayev_a@mail.ru>
2022-12-09 21:08:24 +0500

    feat: add function print-working-directory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import createArgs from './utils/createArguments.js';
+import pwd from './utils/printWorkingDirectory.js';
 import { sayBi, sayHi } from './utils/greetings.js';
 
 const cliArgs = createArgs();
 const fullname = cliArgs['--username'] || 'Student';
 sayHi(fullname);
-sayBi(fullname);
-
+pwd();
 
 
 
---
+++ src/utils/printWorkingDirectory.js
@@ -0,0 +1,4 @@
+import { cwd } from 'process';
+export default function () {
+  process.stdout.write(`You are currently in ${cwd()}`)
+}
